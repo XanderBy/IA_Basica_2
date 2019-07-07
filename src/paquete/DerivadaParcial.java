7ffefd487307feadf0a76c7904f1f85b5d94c4a5
alex <alejandromarquez1998@gmail.com>
2019-07-07 02:38:01 +0200

    derivada parcial

--- src/paquete/DerivadaParcial.java
+++ src/paquete/DerivadaParcial.java
@@ -2,8 +2,24 @@ package paquete;
 
 public class DerivadaParcial {
 
-	
+	/*
+	 * 
+	 * CONSTRUCTOR
+	 * 
+	 */
 	public DerivadaParcial() {
 		
 	}
+	
+	
+	/*
+	 * 
+	 * METODOS
+	 * 
+	 */
+	public static float derivada(float w, float y) {
+		//Derivada Parcial de W(peso de la neurona)
+		return (float) (Math.cos(w+y)-Math.tan(w/y));
+	}
+	
 }
